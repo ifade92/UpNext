a5110a78dada185ba567011e7ef6780903ebbb1b
Carlos Canales <ccanales71@gmail.com>
2026-03-07 22:27:51 -0600

    Initial Commit

---
+++ UpNext/UpNextApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpNextApp.swift
+//  UpNext
+//
+//  Created by Carlos Canales on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct UpNextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UpNextTests/UpNextTests.swift
@@ -0,0 +1,17 @@
+//
+//  UpNextTests.swift
+//  UpNextTests
+//
+//  Created by Carlos Canales on 3/7/26.
+//
+
+import Testing
+@testable import UpNext
+
+struct UpNextTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
